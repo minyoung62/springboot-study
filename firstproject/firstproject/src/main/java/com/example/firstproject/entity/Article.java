a4f601ab59da1a688bedc909cdf4615a922adf68
minyoung62 <tnd05106@naver.com>
2022-03-17 01:45:37 +0900

    데이터 조회하기 with JPA

--- firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 
 @Entity // DB가 해당 객체를 인식 가능
 @AllArgsConstructor
+@NoArgsConstructor // 디폴트 생성자를 추가
 @ToString
 public class Article {
 
