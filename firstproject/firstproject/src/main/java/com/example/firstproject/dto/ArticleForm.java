e96528e248e4d81902a08529f522b24a8497ed95
minyoung62 <tnd05106@naver.com>
2022-03-17 01:28:47 +0900

    롬복과 리팩터링 그리고 로깅 사용

--- firstproject/firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,24 +1,15 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
 
     private String title;
     private String contents;
-
-    public ArticleForm(String title, String contents) {
-        this.title = title;
-        this.contents = contents;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", contents='" + contents + '\'' +
-                '}';
-    }
 
     public Article toEntity(){
         return new Article(null, title, contents);
