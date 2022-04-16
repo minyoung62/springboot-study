c0fbdf8ca015ddd8088c9577fda6d9b7be33f6dc
minyoung62 <tnd05106@naver.com>
2022-04-16 16:51:16 +0900

    querydsl setting

---
+++ firstproject/firstproject/src/main/java/com/example/firstproject/config/QueryDslConfig.java
@@ -0,0 +1,20 @@
+package com.example.firstproject.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+
+@Component
+@RequiredArgsConstructor
+public class QueryDslConfig {
+
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
