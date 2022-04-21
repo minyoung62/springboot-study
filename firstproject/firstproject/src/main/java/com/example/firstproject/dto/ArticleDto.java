1803e8be4434d122f36408a2093f4c05f43566bb
minyoung62 <tnd05106@naver.com>
2022-04-22 04:16:43 +0900

    Dto 추가

---
+++ firstproject/firstproject/src/main/java/com/example/firstproject/dto/ArticleDto.java
@@ -0,0 +1,28 @@
+package com.example.firstproject.dto;
+
+import com.example.firstproject.entity.Article;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ArticleDto {
+
+    private Long id;
+    private String title;
+    private String contents;
+    private String userNickname;
+
+
+    public static ArticleDto createArticleDto(Article article) {
+
+        return ArticleDto.builder()
+                .title(article.getTitle())
+                .contents(article.getContents())
+                .userNickname(article.getUser().getNickname())
+                .id(article.getId())
+                .build();
+    }
+}
